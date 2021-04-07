42fc055b3855d108e87acf1be5038f617efd3e2d
Abdulrahman.Abdulqawi <nizar20172016@gmail.com>
2021-04-07 05:59:45 +0200

    implement Employee controller

--- CloudComputingII_ProjectWork/Models/Employee.cs
+++ CloudComputingII_ProjectWork/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,12 +14,15 @@ namespace CloudComputingII_ProjectWork.Models
         public int  EmployeeId { get; set; }
         [Column("nvarchar(250)")]
         [Required]
+        [DisplayName("Full Name")]
         public string FullName { get; set; }
         [Column("nvarchar(10)")]
+        [DisplayName("Employee Code")]
         public string EmpCode { get; set; }
         [Column("nvarchar(1000)")]
         public string Position { get; set; }
         [Column("nvarchar(100)")]
+        [DisplayName("Office Location")]
         public string OfficeLocation { get; set; }
     }
 }
